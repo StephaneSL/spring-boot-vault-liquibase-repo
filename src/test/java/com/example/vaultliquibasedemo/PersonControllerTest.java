32f4337885a508d071524a363608f91bbb443e6b
fanfanmb <steph70.sl@gmail.com>
2025-10-22 11:36:04 +0200

    commit2 Test file

--- src/test/java/com/example/vaultliquibasedemo/PersonControllerTest.java
+++ src/test/java/com/example/vaultliquibasedemo/PersonControllerTest.java
@@ -1,17 +1,28 @@
 package com.example.vaultliquibasedemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
 import com.example.vaultliquibasedemo.model.Person;
 import com.example.vaultliquibasedemo.repository.PersonRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.http.MediaType;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 
 @SpringBootTest
+@Testcontainers
 @AutoConfigureMockMvc
 class PersonControllerTest {
 
